bfb58685a6e5b0f5903c1fd8f1943ea37c3f4c19
DigitalHatcher <digitalhatcher@gmail.com>
2021-05-13 11:56:43 +0100

    prevState update

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,23 +2,75 @@ import "./ExpenseForm.css";
 import React, { useState } from "react";
 
 const ExpenseForm = () => {
-  const [enteredTitle, setEnteredTitle] = useState("");
-  const [enteredAmount, setEnteredAmount] = useState("");
-  const [enteredDate, setEnteredDate] = useState("");
+  // const [enteredTitle, setEnteredTitle] = useState("");
+  // const [enteredAmount, setEnteredAmount] = useState("");
+  // const [enteredDate, setEnteredDate] = useState("");
+  const [userInput, setUserInput] = useState({
+    enteredTitle: "",
+    enteredAmount: "",
+    enteredDate: "",
+  });
 
+  // const titleChangeHandeler = (event) => {
+  //   setEnteredTitle(event.target.value);
+  //   console.log(event.target.value);
+  // };
+
+  // const amountChangeHandeler = (event) => {
+  //   setEnteredAmount(event.target.value);
+  //   console.log(enteredAmount);
+  // };
+
+  // const dateChangeHandeler = (event) => {
+  //   setEnteredDate(event.target.value);
+  //   console.log(enteredDate);
+  // };
+
+  // React schedules all the state updates so belo approach may lead to issues when there are too many scheduled states, a possibility of using incorrect values i.e. latest version
+  // const titleChangeHandeler = (event) => {
+  //   setUserInput({
+  //     ...userInput,
+  //     enteredTitle: event.target.value,
+  //   });
+  //   console.log(event.target.value);
+  // };
+
+  // const amountChangeHandeler = (event) => {
+  //   setUserInput({
+  //     ...userInput,
+  //     enteredAmount: event.target.value,
+  //   });
+  //   console.log(event.target.value);
+  // };
+
+  // const dateChangeHandeler = (event) => {
+  //   setUserInput({
+  //     ...userInput,
+  //     enteredDate: event.target.value,
+  //   });
+  //   console.log(event.target.value);
+  // };
+
+  // Below approach is correct way to ensure that the right previous state value is used.
   const titleChangeHandeler = (event) => {
-    setEnteredTitle(event.target.value);
+    setUserInput((prevState) => {
+      return { ...prevState, enteredTitle: event.target.value };
+    });
     console.log(event.target.value);
   };
 
   const amountChangeHandeler = (event) => {
-    setEnteredAmount(event.target.value);
-    console.log(enteredAmount);
+    setUserInput((prevState) => {
+      return { ...prevState, enteredAmount: event.target.value };
+    });
+    console.log(event.target.value);
   };
 
   const dateChangeHandeler = (event) => {
-    setEnteredDate(event.target.value);
-    console.log(enteredDate);
+    setUserInput((prevState) => {
+      return { ...prevState, enteredDate: event.target.value };
+    });
+    console.log(event.target.value);
   };
 
   return (
